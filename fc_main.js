8078d83395835c908b5170a380fb27b21afb69c4
the-jay <jaycie@Jays-iMac.local>
2019-03-14 16:57:39 +0100

    default to banking

--- fc_main.js
+++ fc_main.js
@@ -713,7 +713,7 @@ function autoBlacklistOff() {
             FrozenCookies.blacklist = (Game.cookiesEarned >= 1000000000) ? 0 : 2;
             break;
         case 3:
-            FrozenCookies.blacklist = (haveAll('halloween') && haveAll('valentines')) ? 0 : 3;
+            FrozenCookies.blacklist = (haveAll('halloween') && haveAll('valentines')) ? 5 : 3;
             break;
     }
 }
